36739914b00e7a8fdadd90fd9cba09d1fd153097
First-HoWL <154261106+First-HoWL@users.noreply.github.com>
2026-05-05 20:28:21 +0300

    Class Work 05.05, classes: circle and rectangle, pygame

--- main.py
+++ main.py
@@ -1,90 +1,145 @@
 from time import sleep
 import random
+import pygame as pg
 
-class Character:
-    def __init__(self, name, damage, evasion=0, armor=0, lifesteal=0., hp=100.):
-        self.name = name
-        self._hp = hp
-        self.armor = armor
-        self.damage = damage
-        self.evasion = evasion
-        self.lifesteal = lifesteal
 
-    def __str__(self):
-        return f"{self.name} - {self._hp} HP"
-
-    def __repr__(self):
-        return f"{self.name=} \t | {self._hp=} \t | {self.damage=} \t | {self.evasion=} \t | {self.armor=} \t | {self.lifesteal=}"
+class Circle:
+    def __init__(self, color, position, radius):
+        self.position = position
+        self.color = color
+        self.radius = radius
 
     @property
-    def hp(self):
-        return self._hp
+    def radius(self):
+        return self._radius
 
-    @hp.setter
-    def hp(self, value):
-        if value < 0:
-            self._hp = 0
-        else:
-            self._hp = round(value, 3)
-
-
-    def take_damage(self, damage):
-        if random.randint( 1, 100) < self.evasion:
-            return -1
-        else:
-            new_damage = damage - min((damage * (self.armor / 100)), damage)
-            self.hp -= new_damage
-            return new_damage
-
-
-
-    def attack(self, other):
-        if self.hp > 0:
-            damage_modify = self.damage + self.damage * (random.randint(-20, 20) / 100)
-            responce = other.take_damage(damage_modify)
-            if responce != -1:
-                self.hp += (responce / 100) * self.lifesteal
-                return responce
-            else:
-                return -1
-        else:
-            return -2
+    @radius.setter
+    def radius(self, value):
+        self._radius = max(value, 0)
 
     @property
-    def is_alive(self):
-        return self.hp > 0
+    def position(self):
+        return self._position
 
-player = Character("Unit 1", 13, 95)
-player2 = Character("Unit 2", 13,  0,7, 50)
+    @position.setter
+    def position(self, value):
+        self._position = value
 
-while player.is_alive and player2.is_alive:
-    print(player.__repr__())
-    print(player2.__repr__())
+    @property
+    def color(self):
+        return self._color
 
-    attack = player.attack(player2)
+    @color.setter
+    def color(self, value):
+        self._color = value
 
-    if attack == -1:
-        print(f"{player2.name} evaded from attack {player.name}")
-    elif attack == -2:
-        print(player2)
-    else:
-        print(f"{player.name} attacks {player2.name}")
-        print(player2)
-        # sleep(1)
+    def __gt__(self, other):
+        return self.radius > other.radius
 
-    attack = player2.attack(player)
-    if attack == -1:
-        print(f"{player.name} evaded from attack {player2.name}")
-    elif attack == -2:
-        print(player)
-    else:
-        print(f"{player2.name} attacks {player.name}")
-        print(player)
-        # sleep(1)
+    def __ge__(self, other):
+        return self.radius >= other.radius
 
-    print("\n")
-    #sleep(1)
-    if not player.is_alive:
-        print(f"{player2.name} - WIN!")
-    if not player2.is_alive:
-        print(f"{player.name} - WIN!")
+    def __lt__(self, other):
+        return self.radius < other.radius
+
+    def __le__(self, other):
+        return self.radius <= other.radius
+
+    def __add__(self, other):
+        if self >= other :
+            return Circle(self.color, self.position, self.radius + other.radius)
+        else:
+            return Circle(other.color, other.position, self.radius + other.radius)
+    def __sub__(self, other):
+        if self < other:
+            return Exception("First circle is less then seccond")
+        return Circle(self.color, self.position, self.radius - other.radius)
+
+    def draw(self, screen):
+        pg.draw.circle(screen, self.color, self.position, self.radius)
+
+class Rectangle:
+    def __init__(self, color, position, rect):
+        self.position = position
+        self.color = color
+        self.rect = rect
+
+    @property
+    def rect(self):
+        return self._rect
+
+    @rect.setter
+    def rect(self, value):
+        self._rect = (max(value[0], 0), max(value[1], 0))
+
+    @property
+    def position(self):
+        return self._position
+
+    @position.setter
+    def position(self, value):
+        self._position = value
+
+    @property
+    def color(self):
+        return self._color
+
+    @color.setter
+    def color(self, value):
+        self._color = value
+
+    def __gt__(self, other):
+        return self.rect[0] * self.rect[1] > other.rect[0] * other.rect[1]
+
+    def __ge__(self, other):
+        return self.rect[0] * self.rect[1] >= other.rect[0] * other.rect[1]
+
+    def __lt__(self, other):
+        return self.rect[0] * self.rect[1] < other.rect[0] * other.rect[1]
+
+    def __le__(self, other):
+        return self.rect[0] * self.rect[1] <= other.rect[0] * other.rect[1]
+
+    def __add__(self, other):
+        if self >= other :
+            return Rectangle(self.color, self.position, (self.rect[0] + other.rect[0], self.rect[1] + other.rect[1]))
+        else:
+            return Circle(other.color, other.position, (self.rect[0] + other.rect[0], self.rect[1] + other.rect[1]))
+    def __sub__(self, other):
+        if self < other:
+            return Exception("First rectangle is less then seccond")
+        return Rectangle(self.color, self.position, (max(self.rect[0] - other.rect[0], 0), max(self.rect[1] - other.rect[1], 0)))
+
+    def draw(self, screen):
+        pg.draw.rect(screen, self.color, pg.Rect(
+            self.position[0], self.position[1], self.rect[0], self.rect[1]
+        ))
+
+
+
+WINDOW_SIZE = (800, 600)
+def run_game():
+    pg.init()
+    screen = pg.display.set_mode(WINDOW_SIZE)
+    pg.display.set_caption("Game")
+
+    rectangle = Rectangle("white", (300, 200), (200, 200))
+    circle1 = Circle("green", (200,200), 100)
+    circle2 = Circle("red", (100,100), 80)
+    circle3 = circle1 - circle2
+    circle3.color = "blue"
+    print(circle2 > circle1)
+    while True:
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                exit()
+
+        rectangle.draw(screen)
+        circle1.draw(screen)
+        circle2.draw(screen)
+        circle3.draw(screen)
+        pg.display.update()
+
+
+if __name__ == "__main__":
+    run_game()
