5bb0a7fe33f91a5e91cedbddff4cd63c706b8944
First-HoWL <154261106+First-HoWL@users.noreply.github.com>
2026-05-06 18:56:25 +0300

    class berserk

--- main.py
+++ main.py
@@ -115,31 +115,106 @@ class Rectangle:
             self.position[0], self.position[1], self.rect[0], self.rect[1]
         ))
 
+class Character:
+    def __init__(self, name, damage, evasion=0, armor=0, lifesteal=0., hp=100.):
+        self.name = name
+        self._hp = hp
+        self.armor = armor
+        self.damage = damage
+        self.evasion = evasion
+        self.lifesteal = lifesteal
+
+    def __str__(self):
+        return f"{self.name} - {self._hp} HP"
+
+    def __repr__(self):
+        return f"{self.name=} \t | {self._hp=} \t | {self.damage=} \t | {self.evasion=} \t | {self.armor=} \t | {self.lifesteal=}"
+
+    @property
+    def hp(self):
+        return self._hp
+
+    @hp.setter
+    def hp(self, value):
+        if value < 0:
+            self._hp = 0
+        else:
+            self._hp = round(value, 3)
+
+    def take_damage(self, damage):
+        if random.randint( 1, 100) < self.evasion:
+            return -1
+        else:
+            new_damage = damage - min((damage * (self.armor / 100)), damage)
+            self.hp -= new_damage
+            return new_damage
+
+    def attack(self, other):
+        if self.is_alive:
+            damage_modify = self.damage + self.damage * (random.randint(-20, 20) / 100)
+            responce = other.take_damage(damage_modify)
+            if responce != -1:
+                self.hp += (responce / 100) * self.lifesteal
+                return responce
+            else:
+                return -1
+        else:
+            return -2
+
+    @property
+    def is_alive(self):
+        return self.hp > 0
+
+class Berserk(Character):
+    def __init__(self, name, damage, evasion=0, armor=0, lifesteal=0., hp=100.):
+        Character.__init__(self, name, damage, evasion, armor, lifesteal, hp)
+        self._max_hp = self.hp
+
+    def attack(self, other):
+        if self.is_alive:
+            damage_modify = self.damage + self.damage * (random.randint(-20, 20) / 100) + self.damage * (self._max_hp - self.hp) / self._max_hp
+            responce = other.take_damage(damage_modify)
+            if responce != -1:
+                self.hp += (responce / 100) * self.lifesteal
+                return responce
+            else:
+                return -1
+        else:
+            return -2
 
 
-WINDOW_SIZE = (800, 600)
-def run_game():
-    pg.init()
-    screen = pg.display.set_mode(WINDOW_SIZE)
-    pg.display.set_caption("Game")
 
-    rectangle = Rectangle("white", (300, 200), (200, 200))
-    circle1 = Circle("green", (200,200), 100)
-    circle2 = Circle("red", (100,100), 80)
-    circle3 = circle1 - circle2
-    circle3.color = "blue"
-    print(circle2 > circle1)
-    while True:
-        for event in pg.event.get():
-            if event.type == pg.QUIT:
-                exit()
+player = Character("Unit 1", 13, 10, 10, 40)
+player2 = Berserk("Unit 2", 13,  0,7, 0)
 
-        rectangle.draw(screen)
-        circle1.draw(screen)
-        circle2.draw(screen)
-        circle3.draw(screen)
-        pg.display.update()
+while player.is_alive and player2.is_alive:
+    print(player.__repr__())
+    print(player2.__repr__())
 
+    attack = player.attack(player2)
 
-if __name__ == "__main__":
-    run_game()
+    if attack == -1:
+        print(f"{player2.name} evaded from attack {player.name}")
+    elif attack == -2:
+        print(player2)
+    else:
+        print(f"{player.name} attacks {player2.name} with {round(attack, 3)} damage")
+        print(player2)
+        # sleep(1)
+
+    attack = player2.attack(player)
+    if attack == -1:
+        print(f"{player.name} evaded from attack {player2.name} ")
+    elif attack == -2:
+        print(player)
+    else:
+        print(f"{player2.name} attacks {player.name} with {round(attack, 3)} damage")
+        print(player)
+        # sleep(1)
+
+    print("\n")
+    #sleep(1)
+    if not player.is_alive:
+        print(f"{player2.name} - WIN!")
+    if not player2.is_alive:
+        print(f"{player.name} - WIN!")
