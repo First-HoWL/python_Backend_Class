dc401ebb3545d7b0ac3d2f36f6f961c8f83cc702
student <student@L20-03>
2026-05-12 20:43:13 +0300

    Class Work 05.12, Django, MVT, Miadzaki Babadzaki

--- main.py
+++
@@ -1,9 +0,0 @@
-from controller import ShopController
-import sqlite3
-
-if __name__ == '__main__':
-    con = sqlite3.connect('Shop.db')
-
-    controller = ShopController(con)
-    controller.run()
-    con.close()
